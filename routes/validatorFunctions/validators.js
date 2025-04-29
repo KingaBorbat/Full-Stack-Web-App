9d3074f5f0e6e2b94741c27793a8389bac48f4aa
Kinga Borbat <borbat.kinga02@gmail.com>
2025-04-29 19:14:14 +0300

    Add sonar-project.properties and fix an issue.

--- routes/validatorFunctions/validators.js
+++ routes/validatorFunctions/validators.js
@@ -3,30 +3,26 @@
 // year validation: check if it's given and if it's between 1970 and 2024
 export function validateYear(year) {
   const typeYear = typeof year;
-  if (typeYear === "string") {
-    if (year.trim() === "") {
-      return { valid: false, message: "Year field cannot remain empty!" };
+  if (typeYear === 'string') {
+    if (year.trim() === '') {
+      return { valid: false, message: 'Year field cannot remain empty!' };
     }
     const numYear = parseInt(year, 10);
-    if (
-      year.replace(/[0-9]/gi, "").length > 0 ||
-      numYear < 1970 ||
-      numYear > 2024
-    ) {
+    if (year.replace(/[0-9]/gi, '').length > 0 || numYear < 1970 || numYear > 2024) {
       return {
         valid: false,
-        message: "Year must be a number between 1970 and 2024!",
+        message: 'Year must be a number between 1970 and 2024!',
       };
     }
   } else if (year instanceof Number) {
     if (year == null) {
-      return { valid: false, message: "Year field cannot remain empty!" };
+      return { valid: false, message: 'Year field cannot remain empty!' };
     }
 
     if (year < 1970 || year > 2024) {
       return {
         valid: false,
-        message: "Year must be a number between 1970 and 2024!",
+        message: 'Year must be a number between 1970 and 2024!',
       };
     }
   }
@@ -35,13 +31,13 @@ export function validateYear(year) {
 
 // title validation: check if it's given and if it's at least 3 characters long
 export function validateTitle(title) {
-  if (title.trim() === "") {
-    return { valid: false, message: "Title field cannot remain empty!" };
+  if (title.trim() === '') {
+    return { valid: false, message: 'Title field cannot remain empty!' };
   }
   if (title.length < 3) {
     return {
       valid: false,
-      message: "Title name cannot be shorter than 3  characters!",
+      message: 'Title name cannot be shorter than 3  characters!',
     };
   }
   return { valid: true };
@@ -49,13 +45,13 @@ export function validateTitle(title) {
 
 // description validation: check if it's given and if it's at least 50 characters long
 export function validateDescription(description) {
-  if (description.trim() === "") {
-    return { valid: false, message: "Description field cannot remain empty!" };
+  if (description.trim() === '') {
+    return { valid: false, message: 'Description field cannot remain empty!' };
   }
   if (description.length < 50) {
     return {
       valid: false,
-      message: "Description must be at least 50 characters!!",
+      message: 'Description must be at least 50 characters!!',
     };
   }
   return { valid: true };
@@ -63,13 +59,13 @@ export function validateDescription(description) {
 
 // genre validation: checks if it's given and if it's at least 3 characters long
 export function validateGenre(genre) {
-  if (genre.trim() === "") {
-    return { valid: false, message: "Genre field cannot remain empty!!" };
+  if (genre.trim() === '') {
+    return { valid: false, message: 'Genre field cannot remain empty!!' };
   }
   if (genre.length < 3) {
     return {
       valid: false,
-      message: "Genre name cannot be shorter than 3  characters!",
+      message: 'Genre name cannot be shorter than 3  characters!',
     };
   }
   return { valid: true };
@@ -78,14 +74,14 @@ export function validateGenre(genre) {
 // cover image validation: check if there was an image uploaded
 export function validateCoverPhoto(photo) {
   if (!photo || photo.size === 0) {
-    return { valid: false, message: "You must upload a cover photo!" };
+    return { valid: false, message: 'You must upload a cover photo!' };
   }
   return { valid: true };
 }
 
 // validation for search inputs, if they're given
 export function validateFilters(title, genre, minYear, maxYear) {
-  console.log("Validating filters");
+  console.log('Validating filters');
   if (title) {
     const titleValidation = validateTitle(title);
     if (!titleValidation.valid) {
@@ -115,7 +111,7 @@ export function validateFilters(title, genre, minYear, maxYear) {
 
 // validation for new movie inputs: returns false and a message if there's an invalid input
 export function validateInputs(title, description, genre, year, coverPhoto) {
-  console.log("Validate inputs.");
+  console.log('Validate inputs.');
   const titleValidation = validateTitle(title);
   const descriptionValidation = validateDescription(description);
   const genreValidation = validateGenre(genre);
@@ -142,7 +138,7 @@ export function validateInputs(title, description, genre, year, coverPhoto) {
 
 // validation for movie modification
 export function validateEditInputs(title, description, genre, year) {
-  console.log("Validate inputs.");
+  console.log('Validate inputs.');
   const titleValidation = validateTitle(title);
   const descriptionValidation = validateDescription(description);
   const genreValidation = validateGenre(genre);
@@ -164,24 +160,24 @@ export function validateEditInputs(title, description, genre, year) {
 
 // validation for review inputs
 export function validateReviewInputs(points, review) {
-  console.log("Validating review inputs");
+  console.log('Validating review inputs');
   const typePoints = typeof points;
   if (points == null) {
-    return { valid: false, message: "Points field cannot be empty!" };
+    return { valid: false, message: 'Points field cannot be empty!' };
   }
-  if (!typePoints === "number" || points <= 0 || points > 10) {
+  if (!typePoints == 'number' || points <= 0 || points > 10) {
     return {
       valid: false,
-      message: "Points must be a number between 1 and 10!",
+      message: 'Points must be a number between 1 and 10!',
     };
   }
-  if (review.trim() === "") {
-    return { valid: false, message: "Review field cannot be empty!" };
+  if (review.trim() === '') {
+    return { valid: false, message: 'Review field cannot be empty!' };
   }
   if (review.length < 10) {
     return {
       valid: false,
-      message: "Review should contain at least 10 characters!",
+      message: 'Review should contain at least 10 characters!',
     };
   }
   return { valid: true };
